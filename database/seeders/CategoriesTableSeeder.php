31b467e55778a7cdcb227eb15b980018d8f8e22d
Vlad <levchevlad@gmail.com>
2025-02-11 20:20:02 +0200

    added migrations

--- database/seeders/CategoriesTableSeeder.php
+++ database/seeders/CategoriesTableSeeder.php
@@ -12,6 +12,22 @@ class CategoriesTableSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+
+        $categories = [
+            ['category' => 'Programming', 'description' => 'I love programming'],
+            ['category' => 'Swim', 'description' => 'I love swimming'],
+            ['category' => 'Run', 'description' => 'I love running'],
+            ['category' => 'Eat', 'description' => 'I love eating'],
+        ];
+
+        foreach($categories as $category) {
+            \DB::table('categories')->insert([
+                'category' => $category['category'],
+                'description' => $category['description'],
+                'created_at'=>now(),
+                'updated_at'=>now()
+                ]);
+        }
+
     }
 }
--- database/seeders/TagsTableSeeder.php
+++ database/seeders/TagsTableSeeder.php
@@ -12,6 +12,21 @@ class TagsTableSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+
+        $tags = [
+            ['tag' => 'Code'],
+            ['tag' => 'Swim'],
+            ['tag' => 'Run'],
+            ['tag' => 'Eat'],
+        ];
+
+        foreach($tags as $tag) {
+            \DB::table('tags')->insert([
+                'tag' => $tag['tag'],
+                'created_at'=>now(),
+                'updated_at'=>now()
+                ]);
+        }
+
     }
 }
